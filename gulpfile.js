2ba7a4165fae07001cd43d908db7246d23c3b375
Thatiane Pisco <thatianepisco@gmail.com>
2018-08-12 21:28:55 +0100

    Code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var includeSources = require('gulp-include-source');
-var stripCode = require('gulp-strip-code');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function() {
     return gulp.src(['./Source/js/libs/jquery-1.11.3.min.js','./Source/js/libs/qrcode.js','./Source/js/libs/three.min.js','./Source/js/libs/DDSLoader.js','./Source/js/libs/MTLLoader.js','./Source/js/libs/OBJLoader.js','./Source/js/libs/OrbitControls.js','./Source/js/libs/StereoEffect.js','./Source/js/libs/DeviceOrientationControls.js', './Source/js/package.js','./Source/js/app/**/**/!(*.test).js','./Source/js/game.js'])
-        .pipe(sourcemaps.init())
-          .pipe(stripCode({
-            start_comment: "test-code",
-            end_comment: "end-test-code"
-          }))
           .pipe(concat('game.js'))
         	.pipe(rename({suffix: '.min'}))
         	.pipe(uglify())
-        .pipe(sourcemaps.write("maps"))
       	.pipe(gulp.dest('Build/js'));
-});
-
-gulp.task('html', function() {
-  return gulp.src( './Source/index.debug.html' )
-    .pipe( includeSources() )
-    .pipe( gulp.dest('./Build/') );
 });
 
 gulp.task('styles', function () {
@@ -40,7 +23,6 @@ gulp.task('styles', function () {
 
  gulp.task('images', function() {
   return gulp.src('./Source/images/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('./Build/images/'));
 });
 
